5649c14b0692e316534f326f8a65bccb701d982d
lle2022 <lle2022@bu.edu>
2025-11-29 19:26:40 -0500

    New Minimal Adaptive Routing Function. Additionally rename Chaos to DOR, as that is what was implemented. Some bug fixes as well

--- booksim2/src/networks/hexmesh.cpp
+++ booksim2/src/networks/hexmesh.cpp
@@ -1,30 +1,3 @@
-// $Id$
-
-/*
- Copyright (c) 2007-2015, Trustees of The Leland Stanford Junior University
- All rights reserved.
-
- Redistribution and use in source and binary forms, with or without
- modification, are permitted provided that the following conditions are met:
-
- Redistributions of source code must retain the above copyright notice, this 
- list of conditions and the following disclaimer.
- Redistributions in binary form must reproduce the above copyright notice, this
- list of conditions and the following disclaimer in the documentation and/or
- other materials provided with the distribution.
-
- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
- DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
- ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
 /*hexmesh.cpp
  *
  * 6 Neighbour Max Hexagonal Mesh using Row Offsets to stay in Cartesian coordinates
@@ -67,7 +40,8 @@ void HexMesh::_ComputeSize(const Configuration &config)
 }
 
 void HexMesh::RegisterRoutingFunctions() {
-  gRoutingFunctionMap["chaos_hexmesh"] = &chaos_hexmesh;
+  gRoutingFunctionMap["dor_hexmesh"] = &dor_hexmesh;
+  gRoutingFunctionMap["dor_adaptive_hexmesh"] = &dor_adaptive_hexmesh;
 }
   
 void HexMesh::_BuildNet(const Configuration &config)
@@ -94,33 +68,30 @@ void HexMesh::_BuildNet(const Configuration &config)
     // Each router can have up to 7 Input/Output Channels. 6 Neighbours + Inject/eject
     // However we should only create channels that will be used on routers.
     // So first figure out number of In/Out on router
-
     int inputs = 1; //1 Inject Channel
     int outputs = 1; //1 Eject Channel
      for (int dir = 0; dir < 6; ++dir ) {
-       neighbourNode = _DirNode(node, dir);
+       neighbourNode = HexMesh::DirNode(node, dir, gX, gY);
        if (neighbourNode != -1) {
-	 inputs++;
-	 outputs++;
+   inputs++;
+   outputs++;
        }
      }
      
     _routers[node] = Router::NewRouter( config, this, router_name.str( ), 
-					node, inputs, outputs);
-    
-    router_debug << "Router " << _getNodeX(node) << "_" << _getNodeY(node) << " created with (" << _routers[node]->NumInputs()
-     << ") inputs and (" << _routers[node]->NumOutputs() << ") outputs.\n\tConnections:\n";
-    
+          node, inputs, outputs);
     _timed_modules.push_back(_routers[node]);
-
     router_name.str("");
     router_name.clear();
+
+    router_debug << "Router " << _getNodeX(node) << "_" << _getNodeY(node) << " created with (" << _routers[node]->NumInputs()
+     << ") inputs and (" << _routers[node]->NumOutputs() << ") outputs.\n\tConnections:\n";
 
     // Setup channel connections to/from neighbours 
     for (int dir = 0; dir < 6; ++dir ) {
 
       // Find the neighbor 
-      neighbourNode  = _DirNode(node, dir);
+      neighbourNode  = HexMesh::DirNode(node, dir, gX, gY);
 
       // If the neighbour doesn't exist, just continue to next loop
       if (neighbourNode == -1) continue;
@@ -128,13 +99,13 @@ void HexMesh::_BuildNet(const Configuration &config)
       // Get Direction Name for Debug
       string dirName;
       switch (dir) {
-	case 0: dirName = "East"; break;
-	case 1: dirName = "NorthEast"; break;
-	case 2: dirName = "NorthWest"; break;
-	case 3: dirName = "West"; break;
-	case 4: dirName = "SouthWest"; break;
-	case 5: dirName = "SouthEast"; break;
-        }
+        case 0: dirName = "East"; break;
+        case 1: dirName = "NorthEast"; break;
+        case 2: dirName = "NorthWest"; break;
+        case 3: dirName = "West"; break;
+        case 4: dirName = "SouthWest"; break;
+        case 5: dirName = "SouthEast"; break;
+      }
       router_debug << "\t\t" << dirName << "(" << dir << ")" << "\n";
 
       // Get the input channel number
@@ -152,11 +123,11 @@ void HexMesh::_BuildNet(const Configuration &config)
 
       //set input channel latency
       if(use_noc_latency){
-	_chan[inputChannel]->SetLatency( latency );
-	_chan_cred[inputChannel]->SetLatency( latency );
+        _chan[inputChannel]->SetLatency( latency );
+        _chan_cred[inputChannel]->SetLatency( latency );
       } else {
-	_chan[inputChannel]->SetLatency( 27 );
-	_chan_cred[inputChannel]->SetLatency( 27 );
+        _chan[inputChannel]->SetLatency( 1 );
+        _chan_cred[inputChannel]->SetLatency( 1 );
       }
       //get the output channel number
       outputChannel = _DirChannel( node, dir );
@@ -167,11 +138,11 @@ void HexMesh::_BuildNet(const Configuration &config)
 
       //set output channel latency
       if(use_noc_latency){
-	_chan[outputChannel]->SetLatency( latency );
-	_chan_cred[outputChannel]->SetLatency( latency );
+        _chan[outputChannel]->SetLatency( latency );
+        _chan_cred[outputChannel]->SetLatency( latency );
       } else {
-	_chan[outputChannel]->SetLatency( 27 );
-	_chan_cred[outputChannel]->SetLatency( 27 );
+        _chan[outputChannel]->SetLatency( 1 );
+        _chan_cred[outputChannel]->SetLatency( 1 );
       }
     }
     // Add in consideration for input channel
@@ -179,11 +150,11 @@ void HexMesh::_BuildNet(const Configuration &config)
     // Always add Ejection Channel Last
     _routers[node]->AddOutputChannel( _eject[node], _eject_cred[node] );
     // Channel Latencies here set to 1
-    _inject[node]->SetLatency( 27 );
-    _eject[node]->SetLatency( 27 );
+    _inject[node]->SetLatency( 1 );
+    _eject[node]->SetLatency( 1 );
 
     // Output Debug Info
-    cout << router_debug.str() << endl;
+    // cout << router_debug.str() << endl;
   }
 }
 
@@ -197,70 +168,12 @@ int HexMesh::_DirChannel(int node, int dir)
   return ( base + dir );
 }
 
-int HexMesh::_DirNode(int node, int dir)
-{
-  // Get Current XY Equivalent of Node
-  int xcoord = _getNodeX(node); // Current row position
-  int ycoord = _getNodeY(node); // Current column position
-  int difx;
-  int dify;
-
-  // Depending on Direction, dx and dy are different.
-  // 0 - East, 1 - NorthEast, 2 - NorthWest,
-  // 3 - West, 4 - SouthWest, 5 - SouthEast
-
-  // Row Parity Affects Diagonal dX/dY
-  int rowParity = ycoord%2;
-  switch (dir)
-  {
-    case 0:
-      difx = 1;
-      dify = 0;
-      break;
-    case 1:
-      difx = (rowParity==0) ? 0 : 1;
-      dify = 1;
-      break;
-    case 2:
-      difx = (rowParity==0) ? -1 : 0;
-      dify = 1;
-      break;
-    case 3:
-      difx = -1;
-      dify = 0;
-      break;
-    case 4:
-      difx = (rowParity==0) ? -1 : 0;
-      dify = -1;
-      break;
-    case 5:
-      difx = (rowParity==0) ? 0 : 1;
-      dify = -1;
-      break;
-    default:
-      cout << "Error: Unsupported Direction: " << dir << endl;
-      exit (-1);
-  }
-
-  // Calculate new node coords based on direction.
-  int targX = xcoord+difx;
-  int targY = ycoord+dify;
-
-  // If the bounds are exceeded, return -1 to indicate no node exists there
-  if (targY < 0 || targY >= _y) return -1;
-  if (targX < 0 || targX >= _x) return -1;
-
-  // Calculate the node number based on the new target coordinates
-  int targetNode = _getNodeFromXY(targX, targY);
-  return targetNode;
-}
-
 // Static Method for Routing
 int HexMesh::DirNode(int node, int dir, int x, int y)
 {
   // Get Current XY Equivalent of Node
-  int xcoord = node%x; // Current row position
-  int ycoord = node/x; // Current column position
+  int xcoord = node%x; // Current column position
+  int ycoord = node/x; // Current row position
   int difx;
   int dify;
 
@@ -310,18 +223,55 @@ int HexMesh::DirNode(int node, int dir, int x, int y)
   if (targX < 0 || targX >= x) return -1;
 
   // Calculate the node number based on the new target coordinates
-  int targetNode = targX*targY + targX;
+  int targetNode = targY*x + targX;
   return targetNode;
 }
 
+// Static Hexagonal Mesh Distance Calculation
+int HexMesh::HexDistance(int x1, int y1, int x2, int y2) {
+  int dX = x2 - x1;
+  int dY = y2 - y1;
 
-void chaos_hexmesh( const Router *r, const Flit *f, 
-		  int in_channel, OutputSet *outputs, bool inject )
+  // For Even/Odd Rows the dX needs to account for the "staggered rows"
+  if (dY > 0) {
+      // Moving north
+      if (y1 % 2 == 0) dX = dX;  // even row
+      else dX = dX - 1;          // odd row
+  } else if (dY < 0) {
+      // Moving south
+      if (y1 % 2 == 0) dX = dX;  // even row
+      else dX = dX - 1;          // odd row
+  }
+  // Return approximation of how many hops needed
+  return std::max(abs(dX), abs(dY));
+}
+
+// Greedy Minimal Dimensional Order Routing
+void dor_hexmesh( const Router *r, const Flit *f, 
+      int in_channel, OutputSet *outputs, bool inject )
 {
+  int vcBegin = 0, vcEnd = gNumVCs-1;
+
+  if ( f->type == Flit::READ_REQUEST ) {
+    vcBegin = gReadReqBeginVC;
+    vcEnd = gReadReqEndVC;
+  } else if ( f->type == Flit::WRITE_REQUEST ) {
+    vcBegin = gWriteReqBeginVC;
+    vcEnd = gWriteReqEndVC;
+  } else if ( f->type ==  Flit::READ_REPLY ) {
+    vcBegin = gReadReplyBeginVC;
+    vcEnd = gReadReplyEndVC;
+  } else if ( f->type ==  Flit::WRITE_REPLY ) {
+    vcBegin = gWriteReplyBeginVC;
+    vcEnd = gWriteReplyEndVC;
+  }
+
+  assert(((f->vc >= vcBegin) && (f->vc <= vcEnd)) || (inject && (f->vc < 0)));
+
   outputs->Clear( );
 
   if(inject) {
-    outputs->AddRange(-1, 0, 0);
+    outputs->AddRange(-1, vcBegin, vcEnd);
     return;
   }
 
@@ -333,7 +283,7 @@ void chaos_hexmesh( const Router *r, const Flit *f,
   if (cur == dest) {
     int ejectChannel = r->NumOutputs() -1;
     // Ejection Channel should be the 6th channel
-    outputs->AddRange( ejectChannel, 0, 0 );
+    outputs->AddRange( ejectChannel, vcBegin, vcEnd);
     // Exit once this has been found
     return;
   }
@@ -350,7 +300,7 @@ void chaos_hexmesh( const Router *r, const Flit *f,
   // Best Suited Direction
   int dir = -1;
 
-  //Staggered Row implemenetation. so need to know parity
+  //Staggered Row implementation. so need to know parity
   bool even = ((curY % 2) == 0);
 
   // From HexMesh Implementation in hexmesh.cpp:
@@ -374,13 +324,13 @@ void chaos_hexmesh( const Router *r, const Flit *f,
     // And the dx in these cases depends on parity
     if (even) {
       if (dX >= 0) {       
-	dir = 1; // [Even] (0,1) = NorthEast
+        dir = 1; // [Even] (0,1) = NorthEast
       } else {
-	dir = 2; // [Even] (-1,1) = NorthWest
+        dir = 2; // [Even] (-1,1) = NorthWest
       }
     } else {
       if (dX > 0) {
-	dir = 1; // [Odd] (1,1) = NorthEast
+        dir = 1; // [Odd] (1,1) = NorthEast
       } else {
         dir = 2; // [Odd] (0,1) = NorthWest
       }
@@ -397,7 +347,7 @@ void chaos_hexmesh( const Router *r, const Flit *f,
       if (dX > 0) {
         dir = 5; // [Odd] (1,-1) = SouthEast
       } else {
-	dir = 4; // [Odd] (0,-1) = SouthWest
+        dir = 4; // [Odd] (0,-1) = SouthWest
       }
     }
   }
@@ -411,7 +361,116 @@ void chaos_hexmesh( const Router *r, const Flit *f,
   }
 
   // Now we can finally map the output
-  outputs->AddRange(adjustedDir, 0, 0);
+  outputs->AddRange(adjustedDir, vcBegin, vcEnd);
+}
+
+// Adaptive Minimal Dimensional Order Routing
+void dor_adaptive_hexmesh( const Router *r, const Flit *f, 
+      int in_channel, OutputSet *outputs, bool inject )
+{  
+  int vcBegin = 0, vcEnd = gNumVCs-1;
+  if ( f->type == Flit::READ_REQUEST ) {
+    vcBegin = gReadReqBeginVC;
+    vcEnd = gReadReqEndVC;
+  } else if ( f->type == Flit::WRITE_REQUEST ) {
+    vcBegin = gWriteReqBeginVC;
+    vcEnd = gWriteReqEndVC;
+  } else if ( f->type ==  Flit::READ_REPLY ) {
+    vcBegin = gReadReplyBeginVC;
+    vcEnd = gReadReplyEndVC;
+  } else if ( f->type ==  Flit::WRITE_REPLY ) {
+    vcBegin = gWriteReplyBeginVC;
+    vcEnd = gWriteReplyEndVC;
+  }
+  assert(((f->vc >= vcBegin) && (f->vc <= vcEnd)) || (inject && (f->vc < 0)));
+
+  outputs->Clear( );
+
+  if(inject) {
+    outputs->AddRange(-1, vcBegin, vcEnd);
+    return;
+  }
+
+  // Get Current and Target Node Numbers
+  int cur = r->GetID( );
+  int dest = f->dest;
+  
+  // Eject if we reached target
+  if (cur == dest) {
+    int ejectChannel = r->NumOutputs() -1;
+    // Ejection Channel should be the 6th channel
+    outputs->AddRange( ejectChannel,vcBegin, vcEnd);
+    // Exit once this has been found
+    return;
+  }
+
+  // Get Current and Target X/Y coords
+  int curX = cur % gX;
+  int curY = cur / gX;
+  int destX = dest % gX;
+  int destY = dest / gX;
+
+  // Calculate difference for each possible direction
+  std::vector<int> minimalDirs;
+  for (int dir = 0; dir < 6; ++dir ) {
+    int nextNode = HexMesh::DirNode(cur, dir, gX, gY);
+    
+    // If next node is invalid, continue
+    if (nextNode == -1) continue;
+
+    // Next XY Coordinates
+    int nextX = nextNode % gX;
+    int nextY = nextNode / gX;
+
+    // Estimate distance needed to determine if it's a valid direction via rough approximation
+    int new_hops = HexMesh::HexDistance(nextX, nextY, destX, destY);
+    int old_hops = HexMesh::HexDistance(curX, curY, destX, destY);
+
+    // Based on this, we can determine if the direction is a path that approaches the result
+    if (new_hops < old_hops) {
+      minimalDirs.push_back(dir);
+    }
+  }
+
+  // Determine congestion for each of these directions, and use this to determine which one to use.
+  // Base this on the used credits for each direction.
+  int selectedDir = -1;
+  if (!minimalDirs.empty()) {
+    // Select maximum possible integer, so that we get hopefully atleast 1 assignment
+    int minUsed = INT_MAX;
+
+    for (int dir : minimalDirs) {
+      // To correctly find the output port number for each dir, we need to account for the missing connections.
+      int adjustedDir = dir; 
+
+      // Adjust channel number for missing neighbors
+      for (int i = 0; i < dir; ++i) {
+        int tempNeighbour = HexMesh::DirNode(cur, i, gX, gY);
+        if (tempNeighbour == -1) adjustedDir--;
+      }
+      // If underflow occurs, fallback to deterministic
+      if (adjustedDir < 0) {
+        selectedDir = -1;
+        break;
+      }
+
+      // Now calculate used credits
+      int used = r->GetUsedCredit(adjustedDir);
+      if (used < minUsed) {
+        minUsed = used;
+        selectedDir = adjustedDir;
+      }
+    }
+  }
+
+  // Fallback to deterministic patten if no selected selectedDir found
+  if (selectedDir == -1) {
+    dor_hexmesh(r, f, in_channel, outputs, inject);
+    return;
+  }
+
+  // Now we can finally map the output
+  outputs->AddRange(selectedDir, vcBegin, vcEnd);
 }
 
 int HexMesh::_getNodeX (int node)
--- booksim2/src/networks/hexmesh.hpp
+++ booksim2/src/networks/hexmesh.hpp
@@ -1,30 +1,3 @@
-// $Id$
-
-/*
- Copyright (c) 2007-2015, Trustees of The Leland Stanford Junior University
- All rights reserved.
-
- Redistribution and use in source and binary forms, with or without
- modification, are permitted provided that the following conditions are met:
-
- Redistributions of source code must retain the above copyright notice, this 
- list of conditions and the following disclaimer.
- Redistributions in binary form must reproduce the above copyright notice, this
- list of conditions and the following disclaimer in the documentation and/or
- other materials provided with the distribution.
-
- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
- DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
- ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
 #ifndef _HEXMESH_HPP_
 #define _HEXMESH_HPP_
 
@@ -39,7 +12,6 @@ class HexMesh : public Network {
   void _BuildNet (const Configuration &config );
 
   int _DirChannel (int node, int dir);
-  int _DirNode (int node, int dir);
 
   int _getNodeX (int node);
   int _getNodeY (int node);
@@ -52,11 +24,15 @@ public:
   int GetX( ) const;
   int GetY( ) const;
   static int DirNode(int node, int dir, int x, int y); 
+  static int HexDistance(int x1, int y1, int x2, int y2);
 
   double Capacity( ) const;
 };
 
 // Routing Functions
-void chaos_hexmesh( const Router *r, const Flit *f, int in_channel,
+void dor_hexmesh( const Router *r, const Flit *f, int in_channel,
+		    OutputSet *outputs, bool inject );
+
+void dor_adaptive_hexmesh( const Router *r, const Flit *f, int in_channel,
 		    OutputSet *outputs, bool inject );
 #endif
